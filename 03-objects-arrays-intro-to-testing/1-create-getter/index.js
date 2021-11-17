19cce2690066e8cc018cdddb89c82662d44e431a
Alex Bortnik <bortnik.ar@gmail.com>
2021-11-17 19:33:27 +0300

    Implemented task 3-1

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,22 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+  const props = path.split('.');
 
+  return (obj) => {
+    let i = 0;
+
+    while (i < props.length - 1) {
+      const internalObj = obj[props[i]];
+
+      if (!internalObj) {
+        return; // Object doesn't include the specified property
+      }
+
+      obj = internalObj;
+      i++;
+    }
+
+    return obj[props[i]];
+  };
 }
