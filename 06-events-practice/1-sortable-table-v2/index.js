a2508eb6480b5a3da5a71613bd129039f519a88c
Alex Bortnik <bortnik.ar@gmail.com>
2021-11-29 21:42:29 +0300

    Implemented 6-th set of tasks

--- 06-events-practice/1-sortable-table-v2/index.js
+++ 06-events-practice/1-sortable-table-v2/index.js
@@ -1,8 +1,168 @@
 export default class SortableTable {
-  constructor(headersConfig, {
-    data = [],
-    sorted = {}
-  } = {}) {
+  element;
+  subElements = {};
+  sortBy;
+  sortOrder;
 
+  constructor(headersConfig, {
+      data = [],
+      sorted = {},
+    } = {},
+    isSortLocally = true
+  ) {
+    this.headerConfig = headersConfig;
+    this.data = data;
+    this.sortBy = sorted.id;
+    this.sortOrder = sorted.order;
+    this.isSortLocally = isSortLocally;
+
+    this.render();
+    this.sort(this.sortBy, this.sortOrder);
+    this.addEventListeners();
+  }
+
+  render() {
+    const element = document.createElement('div');
+    element.innerHTML = this.getTemplate();
+    this.element = element.firstElementChild;
+    this.subElements = this.getSubElements(element);
+  }
+
+  getTemplate() {
+    const headerRow = this.headerConfig.map(cell => {
+      return `
+        <div class="sortable-table__cell" data-id="${cell.id}" data-sortable="${cell.sortable}">
+          <span>${cell.title}</span>
+          <span data-element="arrow" class="sortable-table__sort-arrow">
+            <span class="sort-arrow"></span>
+          </span>
+        </div>`;
+    }).join('');
+
+    return `
+      <div data-element="productsContainer" class="products-list__container">
+        <div class="sortable-table">
+          <div data-element="header" class="sortable-table__header sortable-table__row">
+            ${headerRow}
+          </div>
+          <div data-element="body" class="sortable-table__body">
+            ${this.buildRows(this.data)}
+          </div>
+        </div>
+      </div>`;
+  }
+
+  buildRows(data) {
+    return data.map(rowData =>
+      `<a href="/products/${rowData.id}" class="sortable-table__row">
+         ${this.buildRow(rowData)}
+       </a>`
+    ).join('');
+  }
+
+  buildRow(rowData) {
+    return this.headerConfig.map(cell => {
+      if (!cell.template) {
+        cell.template = (value) => `<div class="sortable-table__cell">${value}</div>`;
+      }
+
+      return cell.template(rowData[cell.id]);
+    }).join('');
+  }
+
+  sort(field, order) {
+    if (this.isSortLocally) {
+      this.sortOnClient(field, order);
+    } else {
+      this.sortOnServer(field, order);
+    }
+  }
+
+  sortOnClient(field, order) {
+    const fieldConfig = this.headerConfig.find(x => x.id === field);
+    const sortedItems = this.sortItems(this.data, field, fieldConfig.sortType, order);
+    this.update(sortedItems);
+    this.drawHeaderSortArrow(field, order);
+  }
+
+  // TBD: not implemented
+  sortOnServer() { }
+
+  drawHeaderSortArrow(field, order) {
+    const headerCellWithArrow = this.element.querySelector(`.sortable-table__cell[data-order]`);
+    if (headerCellWithArrow) {
+      headerCellWithArrow.removeAttribute('data-order');
+    }
+
+    const currentHeaderCell = this.element.querySelector(`.sortable-table__cell[data-id="${field}"]`);
+    currentHeaderCell.setAttribute('data-order', order);
+  }
+
+  update(data) {
+    this.subElements.body.innerHTML = this.buildRows(data);
+  }
+
+  sortItems(arr, field, type = 'string', order = 'asc') {
+    const compares = {
+      string: (a, b) => a.localeCompare(b, ['ru', 'en'], {caseFirst: 'upper'}),
+      number: (a, b) => a - b
+    };
+    const compare = compares[type];
+
+    const directions = {
+      asc: 1,
+      desc: -1
+    };
+    const direction = directions[order];
+
+    return [...arr].sort((a, b) =>
+      direction * compare(a[field], b[field]));
+  }
+
+  getSubElements(element) {
+    const result = {};
+    const elements = element.querySelectorAll('[data-element]');
+
+    for (const subElement of elements) {
+      const name = subElement.dataset.element;
+      result[name] = subElement;
+    }
+
+    return result;
+  }
+
+  headerCellClicked = (event) => {
+    this.sortBy = event.currentTarget.dataset.id;
+    this.sortOrder = this.sortOrder === 'asc' ? 'desc' : 'asc';
+    this.sort(this.sortBy, this.sortOrder);
+  }
+
+  addEventListeners() {
+    const sortableColumns = this.element.querySelectorAll(
+      '.sortable-table__cell[data-sortable="true"]');
+
+    sortableColumns.forEach(c =>
+      c.addEventListener('pointerdown', this.headerCellClicked));
+  }
+
+  removeEventListeners() {
+    const sortableColumns = this.element.querySelectorAll(
+      '.sortable-table__cell[data-sortable="true"]');
+
+    sortableColumns.forEach(c =>
+      c.removeEventListener('pointerdown', this.headerCellClicked));
+  }
+
+  remove() {
+    if (this.element) {
+      this.removeEventListeners();
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+    this.subElements = {};
   }
 }
--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,5 +1,64 @@
 class Tooltip {
+  static instance;
+  element;
 
+  onPointerMove = (event) => this.moveTo(event.clientX, event.clientY);
+  onPointerOut = () => this.remove();
+
+  onPointerOver = (event) => {
+    const tooltipMessage = event.target.dataset.tooltip;
+    this.render(tooltipMessage);
+
+    this.moveTo(event.clientX, event.clientY);
+    this.tooltipArea.addEventListener('pointermove', this.onPointerMove);
+    this.tooltipArea.addEventListener('pointerout', this.onPointerOut);
+  }
+
+  get tooltipArea() {
+    return document.querySelector('[data-tooltip]');
+  }
+
+  constructor() {
+    if (!Tooltip.instance) {
+      Tooltip.instance = this;
+    }
+    return Tooltip.instance;
+  }
+
+  initialize() {
+    this.tooltipArea.addEventListener('pointerover', this.onPointerOver);
+  }
+
+  render(content = '') {
+    const element = document.createElement('div');
+    element.classList.add('tooltip');
+    element.textContent = content;
+    this.element = element;
+    document.body.prepend(this.element);
+  }
+
+  moveTo(x, y) {
+    this.element.style.left = Math.ceil(x) + 'px';
+    this.element.style.top = Math.ceil(y) + 'px';
+  }
+
+  removeEventListeners() {
+    this.tooltipArea.removeEventListener('pointermove', this.onPointerMove);
+    this.tooltipArea.removeEventListener('pointerover', this.onPointerOver);
+    this.tooltipArea.removeEventListener('pointerout', this.onPointerOut);
+  }
+
+  remove() {
+    if (this.element) {
+      this.removeEventListeners();
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+  }
 }
 
 export default Tooltip;
