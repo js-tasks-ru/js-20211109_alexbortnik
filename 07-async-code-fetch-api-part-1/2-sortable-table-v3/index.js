891e4f394201b6c8a132895f1f7db989a48e3eea
Alex Bortnik <bortnik.ar@gmail.com>
2021-12-03 16:02:29 +0300

    Minor revert for default value of sorting in 7-2

--- 07-async-code-fetch-api-part-1/2-sortable-table-v3/index.js
+++ 07-async-code-fetch-api-part-1/2-sortable-table-v3/index.js
@@ -21,7 +21,7 @@ export default class SortableTable {
 
   constructor(headersConfig, {
       sorted = {},
-      isSortLocally = true,
+      isSortLocally = false,
       url = '',
     } = {}
   ) {
